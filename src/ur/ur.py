1e01f13b5be16eabf55c4b34a0181382019b8092
Mathieu <mathieu@algomus.fr>
2024-01-24 05:24:58 +0100

    ur.py, sacred.py: init, GPLv3+

---
+++ src/ur/ur.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  This file is part of "Ur" <http://www.algomus.fr>,
+#  Co-creative generic music generation
+#  Copyright (C) 2024 by Algomus team at CRIStAL 
+#  (UMR CNRS 9189, Université Lille)
+#
+#  "Ur" is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  "Ur" is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with "Ur". If not, see <http://www.gnu.org/licenses/>
+
