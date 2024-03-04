ca009696cc19cb4cc9757e8f52bfbbf462aaaf38
j-w-moebius <johann.moebius@gmx.net>
2024-03-04 10:34:31 +0100

    make corpus analysis direction- and mode-specific

--- src/analysis/intervals-horizontal.py
+++ src/analysis/intervals-horizontal.py
@@ -1,39 +1,50 @@
 from music21 import *
 import os
 
-# run this script to analyze horizontal (unconditioned) interval frequencies in "The Scared Harp" collection
+# run this script to analyze horizontal interval frequencies in "The Scared Harp" collection
 
 
-def compute_interval_freqs(s: stream.base.Score, freqs_list: list[dict[str, dict[str, int]]]):
-    start = 0
-    if len(s.parts) == 3: # we have: (alto, tenor, bass)
-        start = 1   
-    for (p_ind, p) in enumerate(s.parts, start):
+def compute_interval_freqs(s: stream.base.Score, freqs: dict[str, dict[str, int]], direction: str):
+   
+    for p in s.parts:
         for n in p.flatten().notes:
             succ = n.next("Note")
             if succ == None:
                 continue
             this_pitch = n.pitches[0]
             next_pitch = succ.pitches[0] #if n or succ are chords (i.e., divisi [found rarely and only in octaves], just take the first)
-            
-            if this_pitch not in freqs_list[p_ind].keys():
-                freqs_list[p_ind][this_pitch] = {}
-            successors = freqs_list[p_ind][this_pitch]
-            if next_pitch not in successors.keys():
-                successors[next_pitch] = 1
-            else:
-                successors[next_pitch] = successors[next_pitch] + 1
 
-def normalize_key(s):
-    k = s.analyze('key')
-    if k.mode == 'major':
-        i = interval.Interval(k.tonic, pitch.Pitch('C'))
-    elif k.mode == 'minor':
-        i = interval.Interval(k.tonic, pitch.Pitch('A'))
+            contribute = False
+            if direction == "up":
+                if this_pitch <= next_pitch:
+                    contribute = True
+            elif direction == "down":
+                if this_pitch >= next_pitch:
+                    contribute = True
+
+            if contribute: 
+                if this_pitch not in freqs.keys():
+                    freqs[this_pitch] = {}
+                successors = freqs[this_pitch]
+                if next_pitch not in successors.keys():
+                    successors[next_pitch] = 1
+                else:
+                    successors[next_pitch] = successors[next_pitch] + 1
+
+# transpose the piece to C Major or a minor, depending on its mode
+def normalize_key(s, mode, tonic):
+    if mode == 'major':
+        i = interval.Interval(tonic, pitch.Pitch('C'))
+    elif mode == 'minor':
+        i = interval.Interval(tonic, pitch.Pitch('A'))
     return s.transpose(i)
 
-def analyze_corpus(corpus_path: str) -> list[dict[str, dict[str, float]]]:
-    freqs_list = [{}, {}, {}, {}]
+# analyze horizontal interval frequencies
+# conditioned on one predecessor
+# direction: restricts the pitch pairs that contribute to frequencies ('up' or 'down')
+# mode: restricts the pieces that contribute to frequencies ('major' or 'minor')
+def analyze_corpus(corpus_path: str, mode: str, direction: str) -> dict[str, dict[str, float]]:
+    freqs = {}
 
     print(corpus_path)
     for filename in os.listdir(corpus_path):
@@ -41,33 +52,32 @@ def analyze_corpus(corpus_path: str) -> list[dict[str, dict[str, float]]]:
         print('<==', filename)
         if os.path.isfile(f):
             s = converter.parse(f)
-            normalized_s = normalize_key(s)
-            compute_interval_freqs(normalized_s, freqs_list)
+            k = s.analyze('key')
+            if mode == k.mode:
+                normalized_s = normalize_key(s, k.mode, k.tonic)
+                compute_interval_freqs(normalized_s, freqs, direction)
 
 
     # normalize absolute frequencies to get probabilites
-    for freqs in freqs_list:
-        for (_, p_successors) in freqs.items():
-            s = sum(p_successors.values())
-            for (n, f) in p_successors.items():
-                p_successors[n] = f / s
+    for (_, p_successors) in freqs.items():
+        s = sum(p_successors.values())
+        for (n, f) in p_successors.items():
+            p_successors[n] = f / s
     
-    return freqs_list
+    return freqs
 
-def pretty(freqs_list, nb=15):
-    freqs_sorted = [[(n, sorted(list(succ.items()), key=lambda f:-f[1])) for (n, succ) in freqs.items()] for freqs in freqs_list]
-    s = ''
-    for i, freqs in enumerate(freqs_sorted):
-        s += f'Voice {i + 1}:\n' 
-        for (n, succs) in freqs:
-            s += f'\tSuccessors of {n}:\n'
-            for (data, freq) in succs[:nb]:
-                s += f'\t\t{freq:.3f} {data}\n'
+def pretty(freqs, nb=15):
+    freqs_sorted = [(n, sorted(list(succ.items()), key=lambda f:-f[1])) for (n, succ) in freqs.items()]
+    s = '' 
+    for (n, succs) in freqs_sorted:
+        s += f'\tSuccessors of {n}:\n'
+        for (data, freq) in succs[:nb]:
+            s += f'\t\t{freq:.3f} {data}\n'
 
     return s
 
 
 
 dir_path = os.path.join(os.path.dirname(__file__), '../../data/the-scared-harp')
-freqs = analyze_corpus(dir_path)
+freqs = analyze_corpus(dir_path, 'major', 'down')
 print(pretty(freqs))
