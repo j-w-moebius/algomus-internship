e89dd4e41626ddcd520d05484ab0c3a9e4da6cc2
j-w-moebius <johann.moebius@gmx.net>
2024-03-04 14:40:08 +0100

    hardcode successor frequencies

--- src/analysis/intervals-horizontal.py
+++ src/analysis/intervals-horizontal.py
@@ -4,32 +4,26 @@ import os
 # run this script to analyze horizontal interval frequencies in "The Scared Harp" collection
 
 
-def compute_interval_freqs(s: stream.base.Score, freqs: dict[str, dict[str, int]], direction: str):
+def compute_interval_freqs(s: stream.base.Score, freqs_list: list[dict[str, dict[str, int]]]):
    
-    for p in s.parts:
+    start = 0
+    if len(s.parts) == 3: # we have: (alto, tenor, bass)
+        start = 1   
+    for (p_ind, p) in enumerate(s.parts, start):
         for n in p.flatten().notes:
             succ = n.next("Note")
             if succ == None:
                 continue
             this_pitch = n.pitches[0]
             next_pitch = succ.pitches[0] #if n or succ are chords (i.e., divisi [found rarely and only in octaves], just take the first)
-
-            contribute = False
-            if direction == "up":
-                if this_pitch <= next_pitch:
-                    contribute = True
-            elif direction == "down":
-                if this_pitch >= next_pitch:
-                    contribute = True
-
-            if contribute: 
-                if this_pitch not in freqs.keys():
-                    freqs[this_pitch] = {}
-                successors = freqs[this_pitch]
-                if next_pitch not in successors.keys():
-                    successors[next_pitch] = 1
-                else:
-                    successors[next_pitch] = successors[next_pitch] + 1
+            
+            if this_pitch not in freqs_list[p_ind].keys():
+                freqs_list[p_ind][this_pitch] = {}
+            successors = freqs_list[p_ind][this_pitch]
+            if next_pitch not in successors.keys():
+                successors[next_pitch] = 1
+            else:
+                successors[next_pitch] = successors[next_pitch] + 1
 
 # transpose the piece to C Major or a minor, depending on its mode
 def normalize_key(s, mode, tonic):
@@ -41,10 +35,9 @@ def normalize_key(s, mode, tonic):
 
 # analyze horizontal interval frequencies
 # conditioned on one predecessor
-# direction: restricts the pitch pairs that contribute to frequencies ('up' or 'down')
 # mode: restricts the pieces that contribute to frequencies ('major' or 'minor')
-def analyze_corpus(corpus_path: str, mode: str, direction: str) -> dict[str, dict[str, float]]:
-    freqs = {}
+def analyze_corpus(corpus_path: str, mode: str) -> list[dict[str, dict[str, float]]]:
+    freqs_list = [{}, {}, {}, {}]
 
     print(corpus_path)
     for filename in os.listdir(corpus_path):
@@ -55,35 +48,45 @@ def analyze_corpus(corpus_path: str, mode: str, direction: str) -> dict[str, dic
             k = s.analyze('key')
             if mode == k.mode:
                 normalized_s = normalize_key(s, k.mode, k.tonic)
-                compute_interval_freqs(normalized_s, freqs, direction)
+                compute_interval_freqs(normalized_s, freqs_list)
 
 
     # normalize absolute frequencies to get probabilites
-    for (_, p_successors) in freqs.items():
-        s = sum(p_successors.values())
-        for (n, f) in p_successors.items():
-            p_successors[n] = f / s
+    for freqs in freqs_list:
+        for (_, p_successors) in freqs.items():
+            s = sum(p_successors.values())
+            for (n, f) in p_successors.items():
+                p_successors[n] = f / s
     
-    return freqs
+    return freqs_list
 
 def pretty(freqs, nb=15):
     freqs_sorted = [(n, sorted(list(succ.items()), key=lambda f:str(f[0]))) for (n, succ) in freqs.items()]
 
-    s = '  STATES = [...] \n\n'
+    pitches = [n for (n, _) in freqs_sorted]
+    s = '  STATES = ['
+    su = []
+    for p in pitches:
+        su += [f"'{p}'"]
+    s += ', '. join(su) + ']\n\n'
 
-    s += '  TRANSITIONS = {\n'
-    for (n, succs) in freqs_sorted:
-        s += f"    '{n}': " + '{'
-        su = []
-        for (data, freq) in succs[:nb]:
-            su += [f"'{data}': {freq:.3f}"]
-        s += ', '. join(su) + '},\n'
+    #s += '  TRANSITIONS = {\n'
+    #for (n, succs) in freqs_sorted:
+    #    s += f"    '{n}': " + '{'
+    #    su = []
+    #    for (data, freq) in succs[:nb]:
+    #        su += [f"'{data}': {freq:.3f}"]
+    #    s += ', '. join(su) + '},\n'
 
-    s += '}'
+    #s += '}'
     return s
 
-
+voice_names = ["S", "A", "T", "B"]
 
 dir_path = os.path.join(os.path.dirname(__file__), '../../data/the-scared-harp')
-freqs = analyze_corpus(dir_path, 'major', 'down')
-print(pretty(freqs))
+for mode in ["major", "minor"]:
+    freqs_list = analyze_corpus(dir_path, mode)
+    print(f"{mode}:")
+    for (i, freqs) in enumerate(freqs_list):
+        print(f"Voice {voice_names[i]}:")
+        print(pretty(freqs))
